09655e5f3380c89d11b7aa05b425bba802699b55
Lucas Oliveira <tilucasoli@gmail.com>
2020-11-24 14:57:11 -0300

    ViewCode Settings

--- Make it Great/Model/Activity.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Activity.swift
-//  Make it Great
-//
-//  Created by Lucas Oliveira on 24/11/20.
-//
-
-import Foundation
--- Make it Great/View/ViewController.swift
+++ Make it Great/View/ViewController.swift
@@ -2,14 +2,27 @@
 //  ViewController.swift
 //  Make it Great
 //
-//  Created by Lucas Oliveira on 20/11/20.
+//  Created by Lucas Oliveira on 24/11/20.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
